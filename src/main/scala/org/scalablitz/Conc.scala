57fc268011e09900ebf2d1a6a17f93ee9e57eb36
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2014-02-04 16:55:21 +0100

    Conc tree declarations.

---
+++ src/main/scala/org/scalablitz/Conc.scala
@@ -0,0 +1,39 @@
+package org.scalablitz
+
+
+
+
+
+
+sealed abstract class Conc[+T] {
+  def level: Int
+  def size: Int
+  def normalize = this
+}
+
+
+case object Empty extends Conc[Nothing] {
+  def level = 0
+  def size = 0
+}
+
+
+sealed abstract class Leaf[T] extends Conc[T]
+
+
+case class Single[T](x: T) extends Leaf[T] {
+  def level = 0
+  def size = 1
+}
+
+
+case class Chunk[T](array: Array[T], size: Int) {
+  def level = 0
+}
+
+
+case class <>[T](left: Conc[T], right: Conc[T]) {
+  val level = 1 + math.max(left.level, right.level)
+  val size = left.size + right.size
+}
+
